4f4f6ccab75c21d14c1b60bd51b61b9b11554ced
ClausHofman <46158@edu.raseko.fi>
2022-02-28 14:48:40 +0200

    lisää tehtävä-sivu

--- tehtavat/views.py
+++ tehtavat/views.py
@@ -1,7 +1,11 @@
-from django.http import HttpResponse
+from django.http import (
+    HttpResponse,
+    HttpResponseBadRequest,
+    HttpResponseNotFound,
+)
 from django.shortcuts import render
 
-from tehtavat.models import Tehtava
+from .models import Tehtava
 
 
 def etusivu(request):
@@ -11,10 +15,27 @@ def etusivu(request):
     response = render(request, "etusivu.html", context=tiedot)
     return response
 
+
+def tehtava_sivu(request, id):
+    try:
+        tehtava = Tehtava.objects.get(id=id)
+    except Tehtava.DoesNotExist:
+        return HttpResponseNotFound(f"Tehtävää {id} ei löydy.")
+
+    if request.method == "POST":
+        toiminto = request.POST.get("toiminto")
+        if toiminto == "merkitse_tehdyksi":
+            tehtava.tehty = True
+            tehtava.save()
+        else:
+            return HttpResponseBadRequest("Tuntematon toiminto")
+
+    return render(request, "tehtava.html", context={"tehtava": tehtava})
+
+
 def tietoa(request):
     response = render(request, "tietoa.html")
     return response
 
-
 def yhteystiedot(request):
-    return HttpResponse("<html><body>Yhteystiedot</body></html>")
+    return HttpResponse("<html><body><h1>Yhteystiedot</h1></body></html>")
