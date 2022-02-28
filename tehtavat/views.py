2e0dd7b49991349b6ff71bd72bcc3a09f7343e4d
ClausHofman <46158@edu.raseko.fi>
2022-02-28 11:09:35 +0200

    html asiaa, linkit

--- tehtavat/views.py
+++ tehtavat/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def etusivu(request):
+    response = render(request, "etusivu.html")
+    return response
+
+def tietoa(request):
+    response = render(request, "tietoa.html")
+    return response
+
+
+def yhteystiedot(request):
+    return HttpResponse("<html><body>Yhteystiedot</body></html>")
